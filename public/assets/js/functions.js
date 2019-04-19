fe57e1be75823fce2747a21403528f8a1b179930
shadowolfyt <shadowolfbusiness@gmail.com>
2019-04-19 21:49:19 +1000

    Added a beta version of CSV conversion.

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -26,3 +26,32 @@ function countDown() {
     window.location.href = redirect;
   }
 };
+
+function downloadCSV(csv, filename) {
+  var csvFile;
+  var downloadLink;
+
+  csvFile = new Blob([csv], {type:"text/csv"});
+  downloadLink = document.createElement("a");
+  downloadLink.download = filename;
+  downloadLink.href = window.URL.createObjectURL(csvFile);
+  downloadLink.style.display = "none";
+
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+  var csv = [];
+  var rows = document.querySelectorAll("table tr");
+
+  for(var i = 0; i < rows.length; i++) {
+    var row = [], cols = rows[i].querySelectorAll("td, th");
+    for(var j = 0; j < cols.length; j++)
+    row.push(cols[j].innerText);
+    csv.push(row.join(","));
+  }
+
+  // Download CSV File
+  downloadCSV(csv.join("\n"), filename);
+}
